172ff80f2c36eb160e565917344de3d4917868f2
snehafarkya <snhafarkya@gmail.com>
2022-10-08 19:44:17 +0530

    Put the alert inside the invalidWord id

--- src/script.js
+++ src/script.js
@@ -49,8 +49,10 @@ async function getdata(word) {
   myloader.style.display = "none"; // Hiding the loader
   const statusCode = await response.status;
   if (statusCode != 200) {
-    alert("Please enter a valid alphabet or word");
-    document.querySelector(".main").style.display = "none";
+    document.querySelector(".main").innerHTML=
+    '<h1 id="invalidWord">'
+    alert("Please enter a valid alphabet or word")
+    '</h1>';
   } else {
     for (var i = 0; i < data[0].meanings[0].definitions.length; i++) {
       // Concatening all the meanings of the word
